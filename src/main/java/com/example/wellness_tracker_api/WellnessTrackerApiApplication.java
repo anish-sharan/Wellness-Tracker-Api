ca38c045c99f80a34af411781e13783ff73148e4
anish-sharan <anishsharan2000@gmail.com>
2024-09-12 00:40:38 +0530

    first commit

---
+++ src/main/java/com/example/wellness_tracker_api/WellnessTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.wellness_tracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellnessTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellnessTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/wellness_tracker_api/patient/PatientController.java
@@ -0,0 +1,4 @@
+package com.example.wellness_tracker_api.patient;
+
+public class PatientController {
+}
