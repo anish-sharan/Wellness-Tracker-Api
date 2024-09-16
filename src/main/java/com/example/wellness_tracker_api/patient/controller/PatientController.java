d319c21e81051077a8dd03c9b127edb8b24c253b
anish-sharan <anishsharan2000@gmail.com>
2024-09-17 00:11:02 +0530

    Add - update and query apis

--- src/main/java/com/example/wellness_tracker_api/patient/controller/PatientController.java
+++ src/main/java/com/example/wellness_tracker_api/patient/controller/PatientController.java
@@ -5,11 +5,13 @@ import com.example.wellness_tracker_api.patient.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/patient")
@@ -19,13 +21,34 @@ public class PatientController {
     private PatientService patientService;
 
     @PostMapping
-    private Patient createPatient(@Valid @RequestBody Patient patient){
+    private Patient createPatient(@RequestBody Patient patient){
         return new ResponseEntity<>(patientService.createPatient(patient), HttpStatus.CREATED).getBody();
     }
 
     @GetMapping
     private List<Patient> getPatient(){
         return new ArrayList<>(patientService.getPatient());
+    }
+
+    @GetMapping("/{patientId}")
+    private Optional<Patient> getPatientById(@PathVariable("patientId") String patientId){
+        return patientService.getPatientById(patientId);
+    }
+
+    @GetMapping("/search")
+    public List<Patient> searchPatientByField(@RequestParam(required = false) String firstName,
+                                              @RequestParam(required = false) String lastName) {
+        return patientService.searchPatientByField(firstName, lastName);
+    }
+
+    @PutMapping("/{patientId}")
+    public Patient updatePatient(@PathVariable("patientId") String patientId, @RequestBody Patient updatedPatient) {
+        return patientService.updatePatient(patientId,updatedPatient);
+    }
+
+    @DeleteMapping("/{patientId}")
+    public Optional<Patient> deletePatient(@PathVariable("patientId") String patientId){
+        return patientService.deletePatient(patientId);
     }
 
 }
--- src/main/java/com/example/wellness_tracker_api/patient/service/PatientService.java
+++ src/main/java/com/example/wellness_tracker_api/patient/service/PatientService.java
@@ -1,11 +1,13 @@
 package com.example.wellness_tracker_api.patient.service;
 
 import com.example.wellness_tracker_api.patient.model.Patient;
+import com.example.wellness_tracker_api.patient.model.PatientUpdateDTO;
 import com.example.wellness_tracker_api.patient.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PatientService {
@@ -19,4 +21,31 @@ public class PatientService {
     public List<Patient> getPatient(){
         return patientRepository.findAll();
     }
+
+
+    public Optional<Patient> getPatientById(String patientId) {
+        return patientRepository.findById(patientId);
+    }
+
+    public  List<Patient> searchPatientByField(String firstName, String lastName) {
+        return patientRepository.findByNameField(firstName,lastName);
+    }
+
+    public  Patient updatePatient(String patientId,Patient patientToUpdate) {
+        Optional<Patient> selectedPatient = patientRepository.findById(patientId);
+
+        if (selectedPatient.isPresent()) {
+            Patient patient = selectedPatient.get();
+            patient.setFirstName(patientToUpdate.getFirstName());
+            patient.setLastName(patientToUpdate.getLastName());
+            return patientRepository.save(patient);
+        } else {
+            throw new RuntimeException("Patient not found with ID: " + patientId);
+        }
+    }
+
+    public Optional<Patient> deletePatient(String patientId) {
+        return patientRepository.deleteById(patientId);
+    }
+
 }
